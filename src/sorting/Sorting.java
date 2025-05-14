2de150aa35a584d8ab2d91635b1a405b373c69df
kumavatpooja <poojakumavat232@gmail.com>
2025-05-14 16:36:54 +0530

    Initial commit

---
+++ src/sorting/Sorting.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sorting;
+
+/**
+ *
+ * @author Nugraha
+ */
+public class Sorting {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         // this is the main class and project start from this class
+             new Start().setVisible(true);
+        // TODO code application logic here
+
+        
+    }
+
+    void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
